0b4ec907bbb6c03d2c8e16b2f701f26849302068
hung3102 <daovanhung3102@gmail.com>
2018-09-04 15:57:05 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+require('dotenv').config()
+
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+
+app.post('/webhook', (req, res) => {
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+
+app.get('/webhook', (req, res) => {
+  let VERIFY_TOKEN = process.env.VERIFY_TOKEN
+
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
